7cd88fdac4a7e1717e1565ed68842b86d85e218c
sc2bat <87482415+sc2bat@users.noreply.github.com>
2022-01-30 12:03:42 +0900

    Add files via upload

---
+++ 220127 java/Java100_variable001.java
@@ -0,0 +1,21 @@
+public class Java100_variable001{
+	public static void main(String[] args){
+		System.out.println(3 +4);
+		
+		int a = 3;
+		int b = 4;
+		System.out.println(a + b);
+		
+		a = 4;
+		System.out.println(a);
+		b = 9;
+		System.out.println(a + b);
+		
+		int sum;
+		a = 10;
+		b = 7;
+		sum = a + b;
+		System.out.println(sum);
+		
+	}
+}
